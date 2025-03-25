12ab2272cc29033ba0a6bfb6a29e692c0d6a4dca
Thomas Wening <thomaswening94@gmail.com>
2025-03-26 00:06:39 +0100

    Implement a timer collection.
    - Allows the user to manage several timers with custom durations.
    - The view model exposes a command to add timers and an event for when a timer has completed. When a timer is added, its own `TimerCompleted` event is hooked up to this event.
    - On startup, a default timer is added to the collection.
    - The view model also exposes observable properties for the count of active timers and the next due timer, all of which are updated whenever one of the timers in the collection changes its `IsRunning` property. Also, the duration for the new timer is exposed as a property.
    - When a timer requests deletion, it is removed from the collection of timers in this view model.
    - The view presents the collection of timers as a vertical stack of timer controls filled form top to bottom. In the lower right corner, we have a timer picker to select the time span for new timers and a button to create a new one.
    - Since the time picker control uses `DateTime` internally to store the selected time, I have implemented a `TimeSpanToDateTimeConverter` to seamlessly convert both into one another.
    - Unit tests for the view model are included.

---
+++ PhaseShift.UI/TimerFeature/TimeSpanToDateTimeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PhaseShift.UI.TimerFeature;
+
+internal class TimeSpanToDateTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        if (value is not TimeSpan timeSpan)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        return DateTime.Today + timeSpan;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        if (value is not DateTime dateTime)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        return dateTime.TimeOfDay;        
+    }
+}

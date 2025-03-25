55fe6f814016fe7b0b65208f8f7e2348765f1734
Thomas Wening <thomaswening94@gmail.com>
2025-03-25 20:36:43 +0100

    Implement an `AsyncTimer` to count down from a given duration.
    - It can be started, stopped and reset.
    - Starting the timer starts an internal `AsyncStopwatch` that periodically executes a callback on each tick. At the same time, a while loop also checks on each tick that the stopwatch's elapsed time is less than the timer duration or if the timer has been canceled. If the timer has run out, another callback is executed at the end and the internal stopwatch is reset. If the timer was cancelled, nothing else happens.
    - Stopping the timer stops the internal stopwatch and cancels the token that is passed to the timer loop, causing it to break. Since the timer is just stopped but not reset, starting the timer again will continue where it left off.
    - Resetting the timer simply resets the stopwatch and also cancels the timer loop.
    - The timer duration may also be changed externally, even if the timer is running. This will be handy later on for the pomodoro timer, which shall support adjustments while it is running, without the need to stop and resume.
    - If the timer is not running, the duration is simply set to the new value, given that it is positive. Otherwise, one has to dinstinguish two cases: The new duration is less than the already elapsed time or greater than it. In the first case, the timer is simply stopped and the `_timerCompletedCallback` is executed. In the latter case, the duration is simply adjusted to the new value.
    - I have added unit tests for the timer and I have also added an accuracy test to the accuracy test tool.

---
+++ PhaseShift.Core.Tests/AsyncTimerTests.cs
@@ -0,0 +1,151 @@
+﻿using NSubstitute;
+
+using NUnit.Framework;
+
+namespace PhaseShift.Core.Tests;
+
+[TestFixture]
+internal class AsyncTimerTests
+{
+    private const int CountdownDurationMilliseconds = 1000;
+    private Action? _countdownFinishedCallback;
+    private Action<TimeSpan>? _tickCallback;
+    private AsyncTimer? _asyncTimer;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _countdownFinishedCallback = Substitute.For<Action>();
+        _tickCallback = Substitute.For<Action<TimeSpan>>();
+
+        _asyncTimer = new AsyncTimer(
+            _countdownFinishedCallback,
+            _tickCallback,
+            TimeSpan.FromMilliseconds(CountdownDurationMilliseconds));
+    }
+
+    [Test]
+    public async Task Start_ShouldInvokeTickCallback()
+    {
+        // Arrange
+        _tickCallback!.Invoke(Arg.Any<TimeSpan>());
+
+        // Act
+        _asyncTimer!.Start();
+        await Task.Delay(50); // Allow some time for the async handler to be invoked
+        _asyncTimer.Stop();
+
+        // Assert
+        _tickCallback.Received().Invoke(Arg.Any<TimeSpan>());
+    }
+
+    [Test]
+    public void Stop_ShouldStopTheTimer()
+    {
+        // Act
+        _asyncTimer!.Start();
+        _asyncTimer.Stop();
+
+        // Assert
+        Assert.That(_asyncTimer.IsRunning, Is.False);
+    }
+
+    [Test]
+    public void Reset_ShouldResetElapsedTime()
+    {
+        // Act
+        _asyncTimer!.Start();
+        Task.Delay(50).Wait(); // Allow some time to pass
+        _asyncTimer.Reset();
+
+        // Assert
+        Assert.That(_asyncTimer.ElapsedTime, Is.EqualTo(TimeSpan.Zero));
+    }
+
+    [Test]
+    public void Reset_ShouldInvokeTickCallbackWithZeroElapsedTime()
+    {
+        // Arrange
+        _tickCallback!.Invoke(Arg.Any<TimeSpan>());
+
+        // Act
+        _asyncTimer!.Start();
+        Task.Delay(50).Wait(); // Allow some time to pass
+        _asyncTimer.Reset();
+
+        // Assert
+        _tickCallback.Received().Invoke(TimeSpan.Zero);
+    }
+
+    [Test]
+    public async Task IsRunning_ShouldBeTrueWhenStarted()
+    {
+        // Act
+        _asyncTimer!.Start();
+        await Task.Delay(50); // Allow some time for the async handler to be invoked
+
+        // Assert
+        Assert.That(_asyncTimer.IsRunning, Is.True);
+    }
+
+    [Test]
+    public void IsRunning_ShouldBeFalseWhenStopped()
+    {
+        // Act
+        _asyncTimer!.Start();
+        _asyncTimer.Stop();
+
+        // Assert
+        Assert.That(_asyncTimer.IsRunning, Is.False);
+    }
+
+    [Test]
+    public async Task Timer_ShouldInvokeCountdownFinishedCallback()
+    {
+        // Arrange
+        _countdownFinishedCallback!.Invoke();
+
+        // Act
+        _asyncTimer!.Start();
+        await Task.Delay(CountdownDurationMilliseconds + 100); // Allow enough time for the countdown to finish
+
+        // Assert
+        _countdownFinishedCallback.Received().Invoke();
+    }
+
+    [Test]
+    public async Task ElapsedTime_ShouldBeZeroWhenCountdownFinished()
+    {
+        // Act
+        _asyncTimer!.Start();
+        await Task.Delay(CountdownDurationMilliseconds + 100); // Allow enough time for the countdown to finish
+
+        // Assert
+        Assert.That(_asyncTimer.ElapsedTime, Is.EqualTo(TimeSpan.Zero));
+    }
+
+    [Test]
+    public async Task ElapsedTime_ShouldIncrease_AfterStarting()
+    {
+        // Act
+        _asyncTimer!.Start();
+        await Task.Delay(50); // Allow some time to pass
+        var elapsedTime = _asyncTimer.ElapsedTime;
+
+        // Assert
+        Assert.That(elapsedTime, Is.GreaterThan(TimeSpan.Zero));
+    }
+
+    [Test]
+    public async Task ElapsedTime_ShouldNotBeZero_AfterPausing()
+    {
+        // Act
+        _asyncTimer!.Start();
+        await Task.Delay(100); // Allow some time to pass
+        _asyncTimer.Stop();
+        var elapsedTime = _asyncTimer.ElapsedTime;
+
+        // Assert
+        Assert.That(elapsedTime, Is.GreaterThan(TimeSpan.Zero));
+    }
+}

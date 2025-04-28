a6a8bb2e8c74c9bcb0eddf9d7bba9d0c528a0f17
Thomas Wening <thomaswening94@gmail.com>
2025-04-28 14:58:57 +0200

    Implement a view model for the pomodoro timer.
    - Exposes the timer properties via observable properties. These are updated via the tick callback that is pased to the `PomodoroTimer`.
    - Exposes `RelayCommands` to start, stop and reset the timer, as well as to reset the current phase and reset the whole session or to skip to the next phase.
    - Whenever a phase is completed, the view model issues an `ActiveTimerCommpleted` event with `PomodoroTimerCompletedEventArgs` that contains information on the previous phase and current phase, as well as the number of completed work units, total work units and whether the previous phase was skipped.
    - Unit tests included.

---
+++ PhaseShift.UI.Tests/PomodoroFeature/PomodoroTimerVmTests.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using PhaseShift.Core;
+using PhaseShift.UI.PomodoroFeature;
+using PhaseShift.UI.Tests.Mocks;
+
+namespace PhaseShift.UI.Tests.PomodoroFeature;
+
+[TestFixture]
+internal class PomodoroTimerVmTests
+{
+    protected const int LongBreakDurationSeconds = 1;
+    protected const int TestDelayMilliseconds = 200;
+    protected const int ShortBreakDurationSeconds = 1;
+    protected const int TotalWorkUnits = 2;
+    protected const int WorkDurationSeconds = 1;
+    protected const int WorkUnitsBeforeLongBreak = 1;
+
+    private PomodoroTimerVm? _pomodoroTimerVm;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var settings = new PomodoroSettings
+        {
+            WorkDurationSeconds = WorkDurationSeconds,
+            ShortBreakDurationSeconds = ShortBreakDurationSeconds,
+            LongBreakDurationSeconds = LongBreakDurationSeconds,
+            TotalWorkUnits = TotalWorkUnits,
+            WorkUnitsBeforeLongBreak = WorkUnitsBeforeLongBreak
+        };
+
+        _pomodoroTimerVm = new PomodoroTimerVm(settings, new MockDispatcher());
+    }
+
+    [Test]
+    public void Constructor_InitializesPropertiesCorrectly()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(_pomodoroTimerVm!.CurrentPhase, Is.EqualTo(PomodoroPhase.Work));
+            Assert.That(_pomodoroTimerVm.IsRunning, Is.False);
+            Assert.That(_pomodoroTimerVm.WorkUnitsCompleted, Is.Zero);
+        });
+    }
+
+    [Test]
+    public void UpdateSettings_UpdatesTimersAndSettings()
+    {
+        // Arrange
+        var newSettings = new PomodoroSettings
+        {
+            WorkDurationSeconds = 2,
+            ShortBreakDurationSeconds = 3,
+            LongBreakDurationSeconds = 4,
+            TotalWorkUnits = 5,
+            WorkUnitsBeforeLongBreak = 6
+        };
+
+        // Act
+        _pomodoroTimerVm!.UpdateSettings(newSettings);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(_pomodoroTimerVm.TotalWorkUnits, Is.EqualTo(newSettings.TotalWorkUnits));
+            Assert.That(_pomodoroTimerVm.WorkUnitsBeforeLongBreak, Is.EqualTo(newSettings.WorkUnitsBeforeLongBreak));
+            Assert.That(_pomodoroTimerVm.ShortBreakEqualsLongBreak, Is.EqualTo(newSettings.LongBreakDurationSeconds == newSettings.ShortBreakDurationSeconds));
+        });
+    }
+
+    [Test]
+    public async Task PhaseCompleted_IsInvoked_WhenTimerPassesToNextPhaseOrIsSkipped()
+    {
+        // Arrange
+        bool phaseCompletedInvoked = false;
+        bool phaseWasSkipped = false;
+
+        _pomodoroTimerVm!.ActiveTimerCompleted += (_, args) =>
+        {
+            phaseCompletedInvoked = true;
+            phaseWasSkipped = args.WasSkipped;
+        };
+
+        // Act
+        _pomodoroTimerVm.StartTimerCommand.Execute(null);
+        await Task.Delay(WorkDurationSeconds * 1000 + TestDelayMilliseconds);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(phaseCompletedInvoked, Is.True);
+            Assert.That(phaseWasSkipped, Is.False);
+        });
+
+        // Reset for skip test
+        phaseCompletedInvoked = false;
+        phaseWasSkipped = false;
+
+        // Act  
+        _pomodoroTimerVm.StartTimerCommand.Execute(null);
+        await Task.Delay(TestDelayMilliseconds);
+        _pomodoroTimerVm.SkipToNextPhaseCommand.Execute(null);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(phaseCompletedInvoked, Is.True);
+            Assert.That(phaseWasSkipped, Is.True);
+        });
+    }
+
+    [Test]
+    public async Task PhaseCompleted_IsNotInvoked_WhenActiveTimerIsStoppedOrReset()
+    {
+        // Arrange
+        bool phaseCompletedInvoked = false;
+        _pomodoroTimerVm!.ActiveTimerCompleted += (_, _) => phaseCompletedInvoked = true;
+
+        // Act
+        _pomodoroTimerVm.StartTimerCommand.Execute(null);
+        await Task.Delay(TestDelayMilliseconds);
+        _pomodoroTimerVm.StopTimerCommand.Execute(null);
+
+        // Assert
+        Assert.That(phaseCompletedInvoked, Is.False);
+
+        // Reset for reset test
+        phaseCompletedInvoked = false;
+
+        // Act
+        _pomodoroTimerVm.StartTimerCommand.Execute(null);
+        await Task.Delay(TestDelayMilliseconds);
+        _pomodoroTimerVm.ResetCurrentPhaseCommand.Execute(null);
+
+        // Assert
+        Assert.That(phaseCompletedInvoked, Is.False);
+    }
+
+    [Test]
+    public async Task ResetCurrentPhase_ResetsActiveTimer()
+    {
+        // Arrange
+        TimeSpan activeTimerDuration = _pomodoroTimerVm!.RemainingTime;
+
+        // Act
+        _pomodoroTimerVm.StartTimerCommand.Execute(null);
+        await Task.Delay(TestDelayMilliseconds);
+
+        _pomodoroTimerVm.ResetCurrentPhaseCommand.Execute(null);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(_pomodoroTimerVm.ElapsedTime, Is.EqualTo(TimeSpan.Zero));
+            Assert.That(_pomodoroTimerVm.RemainingTime, Is.EqualTo(activeTimerDuration));
+            Assert.That(_pomodoroTimerVm.IsRunning, Is.False);
+            Assert.That(_pomodoroTimerVm.CurrentPhase, Is.EqualTo(PomodoroPhase.Work));
+        });
+    }
+
+    [Test]
+    public async Task ResetSession_ResetsToWorkPhase()
+    {
+        // Arrange
+        int secondsUntilNextPhase = _pomodoroTimerVm!.RemainingTime.Seconds;
+
+        // Act
+        _pomodoroTimerVm.StartTimerCommand.Execute(null);
+        await Task.Delay(secondsUntilNextPhase * 1000 + TestDelayMilliseconds);
+        _pomodoroTimerVm.ResetSessionCommand.Execute(null);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(_pomodoroTimerVm.CurrentPhase, Is.EqualTo(PomodoroPhase.Work));
+            Assert.That(_pomodoroTimerVm.RemainingTime, Is.EqualTo(TimeSpan.FromSeconds(WorkDurationSeconds)));
+            Assert.That(_pomodoroTimerVm.WorkUnitsCompleted, Is.Zero);
+            Assert.That(_pomodoroTimerVm.IsRunning, Is.False);
+            Assert.That(_pomodoroTimerVm.ElapsedTime, Is.EqualTo(TimeSpan.Zero));
+        });
+    }
+
+    [Test]
+    public async Task SessionCompleted_IsInvoked_WhenAllWorkUnitsAreCompleted()
+    {
+        // Arrange
+        bool sessionCompletedInvoked = false;
+        _pomodoroTimerVm!.ActiveTimerCompleted += (_, _) => sessionCompletedInvoked = true;
+
+        // Act
+        _pomodoroTimerVm.StartTimerCommand.Execute(null);
+        await Task.Delay(_pomodoroTimerVm.TotalTimerDuration.Milliseconds + 2000);
+
+        // Assert
+        Assert.That(sessionCompletedInvoked, Is.True);
+    }
+
+    [Test]
+    public async Task SessionCompleted_IsInvoked_WhenLastWorkUnitIsSkipped()
+    {
+        // Arrange
+        bool sessionCompletedInvoked = false;
+        _pomodoroTimerVm!.ActiveTimerCompleted += (_, _) => sessionCompletedInvoked = true;
+
+        // Act
+        _pomodoroTimerVm.StartTimerCommand.Execute(null);
+        await Task.Delay(_pomodoroTimerVm.TotalTimerDuration.Milliseconds - WorkDurationSeconds * 1000 + 2000);
+        _pomodoroTimerVm.SkipToNextPhaseCommand.Execute(null);
+
+        // Assert
+        Assert.That(sessionCompletedInvoked, Is.True);
+    }
+
+    [Test]
+    public async Task SkipToNextPhase_SkipsToNextPhase()
+    {
+        // Act
+        _pomodoroTimerVm!.StartTimerCommand.Execute(null);
+        await Task.Delay(TestDelayMilliseconds);
+        _pomodoroTimerVm.SkipToNextPhaseCommand.Execute(null);
+        var elapsedTimeAfterSkip = _pomodoroTimerVm.ElapsedTime;
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(_pomodoroTimerVm.CurrentPhase, Is.Not.EqualTo(PomodoroPhase.Work));
+            Assert.That(_pomodoroTimerVm.WorkUnitsCompleted, Is.EqualTo(1));
+            Assert.That(_pomodoroTimerVm.IsRunning, Is.True);
+            Assert.That(elapsedTimeAfterSkip, Is.EqualTo(TimeSpan.Zero).Within(TimeSpan.FromMilliseconds(100)));
+        });
+    }
+
+    [Test]
+    public async Task StartTimer_DoesNotDoAnything_WhenIsRunningIsTrue()
+    {
+        // Act
+        _pomodoroTimerVm!.StartTimerCommand.Execute(null);
+        await Task.Delay(TestDelayMilliseconds);
+        _pomodoroTimerVm.StartTimerCommand.Execute(null);
+
+        // Assert
+        Assert.That(_pomodoroTimerVm.ElapsedTime, Is.Not.EqualTo(TimeSpan.Zero));
+    }
+
+    [Test]
+    public void StartTimer_SetsIsRunningToTrue()
+    {
+        _pomodoroTimerVm!.StartTimerCommand.Execute(null);
+        Assert.That(_pomodoroTimerVm.IsRunning, Is.True);
+    }
+
+    [Test]
+    public void StopTimer_SetsIsRunningToFalse()
+    {
+        // Act
+        _pomodoroTimerVm!.StartTimerCommand.Execute(null);
+        _pomodoroTimerVm.StopTimerCommand.Execute(null);
+
+        // Assert
+        Assert.That(_pomodoroTimerVm.IsRunning, Is.False);
+    }
+
+    [Test]
+    public async Task StopTimer_StopsTheActiveTimer()
+    {
+        // Act
+        _pomodoroTimerVm!.StartTimerCommand.Execute(null);
+        _pomodoroTimerVm.StopTimerCommand.Execute(null);
+        await Task.Delay(TestDelayMilliseconds);
+
+        var elapsedTimeAfterStop = _pomodoroTimerVm.ElapsedTime;
+        await Task.Delay(TestDelayMilliseconds);
+
+        // Assert
+        Assert.That(_pomodoroTimerVm.ElapsedTime, Is.EqualTo(elapsedTimeAfterStop));
+    }
+
+    [Test]
+    public void WorkPhaseIsFollowedByBreakPhase()
+    {
+        // Act
+        _pomodoroTimerVm!.SkipToNextPhaseCommand.Execute(null);
+
+        // Assert
+        Assert.That(_pomodoroTimerVm.CurrentPhase, Is.Not.EqualTo(PomodoroPhase.Work));
+    }
+
+    [Test]
+    public async Task WorkUnitsCompleted_EqualsTotalWorkUnits_WhenSessionCompleted()
+    {
+        // Act
+        _pomodoroTimerVm!.StartTimerCommand.Execute(null);
+        await Task.Delay(_pomodoroTimerVm.TotalTimerDuration.Milliseconds + 3100);
+
+        // Assert
+        Assert.That(_pomodoroTimerVm.WorkUnitsCompleted, Is.EqualTo(TotalWorkUnits));
+    }
+
+    [Test]
+    public async Task WorkUnitsCompleted_IsUpdatedWhenTimerPassesToNextPhase()
+    {
+        // Arrange
+        int secondsUntilNextPhase = _pomodoroTimerVm!.RemainingTime.Seconds;
+
+        // Act
+        _pomodoroTimerVm.StartTimerCommand.Execute(null);
+        await Task.Delay(secondsUntilNextPhase * 1000 + TestDelayMilliseconds);
+
+        // Assert
+        Assert.That(_pomodoroTimerVm.WorkUnitsCompleted, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void TimerInfo_InitializesCorrectly()
+    {
+        // Arrange
+        var info = _pomodoroTimerVm!;
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(info.CurrentPhase, Is.EqualTo(PomodoroPhase.Work));
+            Assert.That(info.ElapsedTime, Is.EqualTo(TimeSpan.Zero));
+            Assert.That(info.IsRunning, Is.False);
+            Assert.That(info.TotalWorkUnits, Is.EqualTo(TotalWorkUnits));
+            Assert.That(info.WorkUnitsBeforeLongBreak, Is.EqualTo(WorkUnitsBeforeLongBreak));
+            Assert.That(info.WorkUnitsCompleted, Is.EqualTo(0));
+        });
+    }
+
+    [Test]
+    public async Task TimerInfo_UpdatesCorrectly_AfterStarting()
+    {
+        // Act
+        _pomodoroTimerVm!.StartTimerCommand.Execute(null);
+        await Task.Delay(TestDelayMilliseconds);
+
+        // Assert
+        var info = _pomodoroTimerVm;
+        Assert.Multiple(() =>
+        {
+            Assert.That(info.CurrentPhase, Is.EqualTo(PomodoroPhase.Work));
+            Assert.That(info.ElapsedTime, Is.GreaterThan(TimeSpan.Zero));
+            Assert.That(info.IsRunning, Is.True);
+            Assert.That(info.RemainingTime, Is.LessThan(TimeSpan.FromSeconds(WorkDurationSeconds)));
+            Assert.That(info.TotalElapsedTime, Is.GreaterThan(TimeSpan.Zero));
+            Assert.That(info.TotalRemainingTime, Is.LessThan(info.TotalTimerDuration));
+            Assert.That(info.WorkUnitsCompleted, Is.EqualTo(0));
+        });
+    }
+
+    [Test]
+    public async Task TimerInfo_ResetsCorrectly()
+    {
+        // Act
+        _pomodoroTimerVm!.StartTimerCommand.Execute(null);
+        await Task.Delay(TestDelayMilliseconds);
+        _pomodoroTimerVm.ResetCurrentPhaseCommand.Execute(null);
+
+        // Assert
+        var info = _pomodoroTimerVm;
+        Assert.Multiple(() =>
+        {
+            Assert.That(info.CurrentPhase, Is.EqualTo(PomodoroPhase.Work));
+            Assert.That(info.ElapsedTime, Is.EqualTo(TimeSpan.Zero));
+            Assert.That(info.IsRunning, Is.False);
+            Assert.That(info.RemainingTime, Is.EqualTo(TimeSpan.FromSeconds(WorkDurationSeconds)));
+            Assert.That(info.TotalElapsedTime, Is.EqualTo(TimeSpan.Zero));
+            Assert.That(info.TotalRemainingTime, Is.EqualTo(info.TotalTimerDuration));
+            Assert.That(info.WorkUnitsCompleted, Is.EqualTo(0));
+        });
+    }
+}
---
+++ PhaseShift.UI/PomodoroFeature/PomodoroVm.cs
@@ -0,0 +1,159 @@
+﻿using System.Windows.Threading;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using PhaseShift.Core;
+
+using PhaseShift.UI.Common;
+
+namespace PhaseShift.UI.PomodoroFeature;
+internal partial class PomodoroTimerVm : PageViewModel
+{
+    private readonly IDispatcher _dispatcher;
+    private readonly PomodoroTimer _pomodoroTimer;
+
+    [ObservableProperty]
+    private TimeSpan _elapsedTime;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(StartTimerCommand))]
+    private bool _isRunning;
+
+    [ObservableProperty]
+    private double _progressCurrentPhase;
+
+    [ObservableProperty]
+    private TimeSpan _remainingTime;
+
+    [ObservableProperty]
+    private TimeSpan _totalElapsedTime;
+
+    [ObservableProperty]
+    private int _totalWorkUnits;
+
+    [ObservableProperty]
+    private int _workUnitsBeforeLongBreak;
+
+    [ObservableProperty]
+    private int _workUnitsCompleted;
+
+    [ObservableProperty]
+    private PomodoroPhase _currentPhase;
+
+    [ObservableProperty]
+    private bool _shortBreakEqualsLongBreak;
+
+    [ObservableProperty]
+    private TimeSpan _totalTimerDuration;
+
+    [ObservableProperty]
+    private TimeSpan _totalRemainingTime;
+
+    public PomodoroTimerVm(PomodoroSettings? settings = null, IDispatcher? dispatcher = null)
+    {
+        // For designer, use a default dispatcher
+        _dispatcher = dispatcher ?? new DispatcherWrapper(System.Windows.Application.Current.Dispatcher);
+
+        settings ??= new PomodoroSettings();
+        _pomodoroTimer = new PomodoroTimer(() => _dispatcher.Invoke(UpdateTimerState), settings);
+        _pomodoroTimer.PhaseCompleted += OnPhaseCompleted;
+
+        CurrentPhase = _pomodoroTimer.Info.CurrentPhase;
+        WorkUnitsCompleted = _pomodoroTimer.Info.WorkUnitsCompleted;
+        TotalTimerDuration = _pomodoroTimer.Info.TotalTimerDuration;
+        UpdateTimerState();
+
+        TotalWorkUnits = _pomodoroTimer.Info.Settings.TotalWorkUnits;
+        WorkUnitsBeforeLongBreak = _pomodoroTimer.Info.Settings.WorkUnitsBeforeLongBreak;
+        ShortBreakEqualsLongBreak = _pomodoroTimer.Info.Settings.LongBreakDurationSeconds == _pomodoroTimer.Info.Settings.ShortBreakDurationSeconds;
+    }
+
+    public event EventHandler<PomodoroTimerCompletedEventArgs>? ActiveTimerCompleted;
+    public event EventHandler? PomodoroSettingsRequested;
+
+    public override string Title => "Pomodoro Timer";
+
+    [RelayCommand]
+    public void StopTimer()
+    {
+        _pomodoroTimer.StopActiveTimer();
+        IsRunning = _pomodoroTimer.IsRunning;
+    }
+
+    private bool CanStartActiveTimer() => !_pomodoroTimer.IsRunning;
+
+    [RelayCommand]
+    private void EditPomodoroSettings()
+    {
+        PomodoroSettingsRequested?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void OnPhaseCompleted(object? sender, bool wasSkipped)
+    {
+        WorkUnitsCompleted = _pomodoroTimer.Info.WorkUnitsCompleted;
+        IsRunning = _pomodoroTimer.IsRunning;
+
+        var oldPhase = CurrentPhase;
+        CurrentPhase = _pomodoroTimer.Info.CurrentPhase;
+
+        var args = new PomodoroTimerCompletedEventArgs(oldPhase, CurrentPhase, WorkUnitsCompleted, TotalWorkUnits, wasSkipped);
+        ActiveTimerCompleted?.Invoke(this, args);
+    }
+
+    [RelayCommand]
+    private void ResetCurrentPhase()
+    {
+        _pomodoroTimer.ResetActiveTimer();
+        IsRunning = _pomodoroTimer.IsRunning;
+    }
+
+    [RelayCommand]
+    private void ResetSession()
+    {
+        _pomodoroTimer.ResetSession();
+        WorkUnitsCompleted = _pomodoroTimer.Info.WorkUnitsCompleted;
+        CurrentPhase = _pomodoroTimer.Info.CurrentPhase;
+        IsRunning = _pomodoroTimer.IsRunning;
+
+        UpdateTimerState();
+    }
+
+    [RelayCommand]
+    private void SkipToNextPhase()
+    {
+        _pomodoroTimer.SkipActiveTimer();
+        IsRunning = _pomodoroTimer.IsRunning;
+    }
+
+    [RelayCommand(CanExecute = nameof(CanStartActiveTimer))]
+    private void StartTimer()
+    {
+        if (WorkUnitsCompleted >= _pomodoroTimer.Info.Settings.TotalWorkUnits)
+        {
+            _pomodoroTimer.ResetSession();
+        }
+
+        _pomodoroTimer.StartActiveTimer();
+        IsRunning = _pomodoroTimer.IsRunning;
+    }
+
+    private void UpdateTimerState()
+    {
+        RemainingTime = _pomodoroTimer.Info.RemainingTimeInCurrentPhase;
+        ElapsedTime = _pomodoroTimer.Info.ElapsedTimeInCurrentPhase;
+        TotalElapsedTime = _pomodoroTimer.Info.TotalElapsedTime;
+        ProgressCurrentPhase = _pomodoroTimer.Info.ProgressCurrentPhase;
+        TotalRemainingTime = _pomodoroTimer.Info.TotalRemainingTime;
+    }
+
+    public void UpdateSettings(PomodoroSettings settings)
+    {
+        _pomodoroTimer.UpdateSettings(settings);
+
+        TotalTimerDuration = _pomodoroTimer.Info.TotalTimerDuration;
+        ShortBreakEqualsLongBreak = _pomodoroTimer.Info.Settings.LongBreakDurationSeconds == _pomodoroTimer.Info.Settings.ShortBreakDurationSeconds;
+        TotalWorkUnits = _pomodoroTimer.Info.Settings.TotalWorkUnits;
+        WorkUnitsBeforeLongBreak = _pomodoroTimer.Info.Settings.WorkUnitsBeforeLongBreak;
+    }
+}

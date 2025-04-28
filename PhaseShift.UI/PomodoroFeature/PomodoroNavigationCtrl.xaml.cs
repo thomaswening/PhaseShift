3fd86c521e5e834d7094f13c755861bd6a182231
Thomas Wening <thomaswening94@gmail.com>
2025-04-28 21:35:13 +0200

    Add navigation between timer and settings.
    - Navigation view model exposes the current view model (settings or pomodoro timer) as an observable property, as well as its title.
    - Exposes events `TimerCompleted` using `PomodoroTimerCompletedEventArgs` for whenever an active timer in `PomodoroTimerVm` has completed, i.e. phase completed or session completed, as well as `StatusChanged` for when the timer is stopped/started, the current phase has changed or the remaining time has changed.
    - Switches between `PomodoroTimerVm`, `PomodoroSettingsVm` when one of their events  `PomodoroSettingsRequested`, `TimerRequested` is invoked.
    - Corresponding view `PomodoroNavigationCtrl` only contains a content control with data templates for both view models.
    - Included tests for `PomodoroNavigationVm`.

---
+++ PhaseShift.UI/PomodoroFeature/PomodoroNavigationCtrl.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace PhaseShift.UI.PomodoroFeature;
+/// <summary>
+/// Interaction logic for PomodoroNavigationCtrl.xaml
+/// </summary>
+public partial class PomodoroNavigationCtrl : System.Windows.Controls.UserControl
+{
+    public PomodoroNavigationCtrl()
+    {
+        InitializeComponent();
+    }
+}
---
+++ PhaseShift.UI/PomodoroFeature/PomodoroNavigationVm.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+using PhaseShift.Core;
+using PhaseShift.UI.Common;
+
+using PhaseShift.UI.PomodoroFeature.Settings;
+
+namespace PhaseShift.UI.PomodoroFeature;
+
+internal partial class PomodoroNavigationVm : PageViewModel
+{
+    public PomodoroTimerVm TimerVm { get; private set; }
+    public PomodoroSettingsVm SettingsVm { get; init; }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Title))]
+    private PageViewModel _currentViewModel;
+
+    public override string Title => CurrentViewModel.Title;
+
+    public event EventHandler<PomodoroTimerCompletedEventArgs>? TimerCompleted;
+    public event EventHandler? StatusChanged;
+
+    public PomodoroNavigationVm(IDispatcher? dispatcher = null)
+    {
+        dispatcher ??= new DispatcherWrapper(System.Windows.Application.Current.Dispatcher);
+
+        var timerVm = CreatePomodoroTimerVm(dispatcher);
+        TimerVm = timerVm;
+
+        SettingsVm = new PomodoroSettingsVm();
+        SettingsVm.TimerRequested += OnTimerRequested;
+        SettingsVm.SettingsChanged += OnSettingsChanged;
+
+        CurrentViewModel = TimerVm;
+    }
+
+    private PomodoroTimerVm CreatePomodoroTimerVm(IDispatcher dispatcher, PomodoroSettings? settings = null)
+    {
+        var timerVm = new PomodoroTimerVm(settings, dispatcher);
+        timerVm.ActiveTimerCompleted += OnActiveTimerCompleted;
+        timerVm.PomodoroSettingsRequested += OnPomodoroSettingsRequested;
+        timerVm.PropertyChanged += OnPropertyChanged;
+        return timerVm;
+    }
+
+    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(PomodoroTimerVm.IsRunning)
+            || e.PropertyName == nameof(PomodoroTimerVm.CurrentPhase)
+            || e.PropertyName == nameof(PomodoroTimerVm.RemainingTime))
+        {
+            StatusChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    private void OnActiveTimerCompleted(object? sender, PomodoroTimerCompletedEventArgs e)
+    {
+        TimerCompleted?.Invoke(sender, e);
+    }
+
+    private void OnSettingsChanged(object? sender, PomodoroSettings settings)
+    {
+        TimerVm.UpdateSettings(settings);
+    }
+
+    private void OnTimerRequested(object? sender, EventArgs e)
+    {
+        CurrentViewModel = TimerVm;
+    }
+
+    private void OnPomodoroSettingsRequested(object? sender, EventArgs e)
+    {
+        CurrentViewModel = SettingsVm;
+    }
+}

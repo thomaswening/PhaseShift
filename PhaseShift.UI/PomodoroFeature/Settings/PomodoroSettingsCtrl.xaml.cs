2c030cece95765e996cfdc5fc2f6fc95875ffcaa
Thomas Wening <thomaswening94@gmail.com>
2025-04-28 15:19:44 +0200

    Add a view model for the pomodoro settings.
    - Exposes the settings properties as observable properties.
    - Control offers various sliders to adjust the pomodoro settings. Upper and lower bounds are dynamically set to avoid faulty configurations.

---
+++ PhaseShift.UI/PomodoroFeature/Settings/PomodoroSettingsCtrl.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PhaseShift.UI.PomodoroFeature.Settings;
+/// <summary>
+/// Interaction logic for PomodoroSettingsCtrl.xaml
+/// </summary>
+public partial class PomodoroSettingsCtrl : System.Windows.Controls.UserControl
+{
+    public PomodoroSettingsCtrl()
+    {
+        InitializeComponent();
+    }
+}

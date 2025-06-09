915e67a363fd5522bb068fb04f7a4b75d885d386
Thomas Wening <thomaswening94@gmail.com>
2025-06-09 21:13:07 +0200

    Add necessary metadata for release.
    - Moved app name and app version to dedicated static class for app constants. However, these must also be adapted in the csproj file. I have added respective comments. Not the idea solution but pragmatic. I am going to do some dev ops on this project in the future.

---
+++ PhaseShift.UI/AppConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace PhaseShift.UI;
+internal static class AppConstants
+{
+    public static string AppName => "PhaseShift"; // must also be changed in csproj
+    public static string AppVersion => "0.3.1"; // must also be changed in csproj
+}
